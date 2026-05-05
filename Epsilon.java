36991a734a75de41cfcf904b824c9d01fba5c19f
Joshua <112779066+Jxshua17@users.noreply.github.com>
2026-05-05 14:32:09 +0100

    Delete Epsilon.java

--- Epsilon.java
+++
@@ -1,7 +0,0 @@
-package MathStuff;
-
-public class Epsilon {
-    public static void main(String[] args) {
-        System.out.println(String.valueOf(Math.pow(7, 2) * Math.pow(2, 22)).length());
-    }
-}
