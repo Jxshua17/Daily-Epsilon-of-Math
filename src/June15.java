53f980f0223e8aeb0429f4fb92972fc64d5e268a
Joshua <112779066+Jxshua17@users.noreply.github.com>
2026-07-02 17:00:13 +0100

    added the file containing the java program for June 15

--- src/June15.java
+++ src/June15.java
@@ -1,19 +1,27 @@
 public class June15 {
 
     public static void main(String[] args) {
-        System.out.println("Cooool out!");
         String number = "23A8";
-        char ch = number.charAt(2);
-        System.out.println(ch);
 
-        String x = "X";
-        String name = "Jxshua";
-        char n = name.charAt(1);
+        int sum = 0;
 
-        String newName = name.replace('x', 'o');
-        System.out.println(n);
-        System.out.println(newName);
-        //char chNo = Character.valueOf(x);
-        //System.out.println(chNo);
+        for (int i = 0; i <= 9; i++) {
+            //You have to turn i into a string so that A in number can be replaced to perform the calculation.
+            String y = String.valueOf(i);
+            //Replacing A with the current number , i, in the loop.
+            String newNumb = number.replace('A', y.charAt(0));
+            //Turning newNumb to an integer because it is currently a string.
+            int no = Integer.parseInt(newNumb);
+
+            //checking if newNumb which is now stored in no, is divisible by 6 and if the condition is true, we add it to the sum variable.
+            if (no % 6 == 0) {
+                sum = sum + i;
+            }
+        }
+
+        System.out.println(
+            "the sum of all possible digits A such that the number 23A8 is divisible by 6 is " +
+                sum
+        );
     }
 }
